3a2764c592b2ae18980c97f8c597beb8f221cf50
zongzhec <zongzhe_chen@sina.com>
2020-04-26 11:28:10 +0800

    Enhance WordCount Demo

--- src/main/java/foo/zongzhe/map_reduce/word_count/WordCountDemo.java
+++ src/main/java/foo/zongzhe/map_reduce/word_count/WordCountDemo.java
@@ -1,8 +1,16 @@
 package foo.zongzhe.map_reduce.word_count;
 
+import java.io.IOException;
+
 /**
  * A demo uses mapreduce to count words in a txt file.
+ *
  * @author Zongzhe
+ * 此Demo类可以被WordCountDriver完美代替，只是为了遵循本项目下的惯例，固此处仍然用Demo做入口。
  */
 public class WordCountDemo {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        WordCountDriver wd = new WordCountDriver();
+        wd.wcDriver(args);
+    }
 }
--- src/main/java/foo/zongzhe/map_reduce/word_count/WordCountDriver.java
+++ src/main/java/foo/zongzhe/map_reduce/word_count/WordCountDriver.java
@@ -1,4 +1,47 @@
 package foo.zongzhe.map_reduce.word_count;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Driver 的套路化非常突出，因此也是最好写的一个类
+ */
 public class WordCountDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        WordCountDriver wd = new WordCountDriver();
+        wd.wcDriver(args);
+    }
+
+    public void wcDriver(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        // 获取一个MR job实例
+        Job job = Job.getInstance(new Configuration());
+
+        // 设置类路径
+        job.setJarByClass(WordCountDriver.class);
+
+        // 设置Mapper和Reducer
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        // 设置Mapper和Reducer的输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 设置输入输出数据
+        FileInputFormat.setInputPaths(job, new Path(args[0])); //FileInputFormat导包不要导错了
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 提交job
+        boolean success = job.waitForCompletion(true);
+        System.exit(success ? 0 : 1);
+    }
 }
